b37f8ae9698626585a440750f161580adcb91f3e
Tomasz <tomasz@tomasz-VirtualBox.(none)>
2013-10-15 22:34:50 +0100

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,12 @@ Webdesignstories::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'whispering-bayou-9695.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
 
