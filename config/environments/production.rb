9a8bc134fb0e60d761c3fadfe6166db17ba17344
Tomasz <tomasz@tomasz-VirtualBox.(none)>
2013-10-17 14:57:16 +0100

    hello

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Webdesignstories::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'webdesignstories.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
