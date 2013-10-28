40fad234b0c1a2b37077f0be8c0b69dd50c8868f
Thomas <thomas@thomas-VirtualBox.(none)>
2013-10-28 21:58:45 +0000

    helloe

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+  require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -69,5 +69,6 @@ module Webdesignstories
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
