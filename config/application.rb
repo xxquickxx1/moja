379128afe29bb2d0b17eb53f9f64a59e5bcec5bd
Thomas <thomas@thomas-VirtualBox.(none)>
2013-10-28 22:17:29 +0000

    hello

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Webdesignstories
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-
+config.assets.initialize_on_precompile = false
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
